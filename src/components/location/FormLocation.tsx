caf4946ae95d64f71de9d7ee85334b713d55a01c
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-12-02 17:28:58 +0300

    select locataire

--- src/components/location/FormLocation.tsx
+++ src/components/location/FormLocation.tsx
@@ -1,11 +1,16 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Button, View} from 'react-native';
 import SectionTitle from '../../components/public/SectionTitle';
 import themes from '../../theme';
+import {Picker} from '@react-native-picker/picker';
+import MyTextInput from '../public/MyTextInput';
+import tailwind from 'tailwind-rn';
 
 const FormLocation: FC<{
   type: 'add' | 'update';
 }> = ({type}) => {
+  const [state, setState] = useState('Rakoto');
+
   return (
     <View>
       <SectionTitle
@@ -13,6 +18,20 @@ const FormLocation: FC<{
         text={type === 'add' ? 'Ajout de location' : 'Modifier la location'}
       />
       <View>
+        <View>
+          <Picker
+            selectedValue={state}
+            onValueChange={function (itemValue) {
+              setState(itemValue as string);
+            }}>
+            <Picker.Item label="Rakoto" value="Rakoto" />
+            <Picker.Item label="Rabe" value="Rabe" />
+          </Picker>
+          <View>
+            <MyTextInput value="" placeholder="Nombre de jour" />
+          </View>
+        </View>
+
         <Button
           onPress={function () {}}
           title={type === 'add' ? 'ajouter' : 'modifier'}
