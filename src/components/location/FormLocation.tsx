68bd90e2a1d8147cc3e1bd1841c32c25178e99e1
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-12-02 17:53:13 +0300

    date picker

--- src/components/location/FormLocation.tsx
+++ src/components/location/FormLocation.tsx
@@ -1,15 +1,18 @@
 import React, {FC, useState} from 'react';
-import {Button, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import SectionTitle from '../../components/public/SectionTitle';
 import themes from '../../theme';
 import {Picker} from '@react-native-picker/picker';
 import MyTextInput from '../public/MyTextInput';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import tailwind from 'tailwind-rn';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const FormLocation: FC<{
   type: 'add' | 'update';
 }> = ({type}) => {
   const [state, setState] = useState('Rakoto');
+  const [showDate, setShowDate] = useState(false);
 
   return (
     <View>
@@ -21,14 +24,42 @@ const FormLocation: FC<{
         <View>
           <Picker
             selectedValue={state}
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{width: '50%'}}
             onValueChange={function (itemValue) {
               setState(itemValue as string);
             }}>
             <Picker.Item label="Rakoto" value="Rakoto" />
             <Picker.Item label="Rabe" value="Rabe" />
           </Picker>
-          <View>
-            <MyTextInput value="" placeholder="Nombre de jour" />
+          <View style={tailwind('flex-row items-center')}>
+            <View style={tailwind('flex-1')}>
+              <MyTextInput
+                value=""
+                placeholder="Nombre de jour"
+                keyboardType="numeric"
+              />
+            </View>
+            <Text style={tailwind('flex-1')}>
+              <Ionicons
+                name="calendar"
+                size={themes.space * 2}
+                onPress={() => setShowDate(true)}
+              />
+              {'    '}
+              {'date de location'}
+            </Text>
+            {showDate && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={new Date()}
+                is24Hour={true}
+                display="default"
+                onChange={function () {
+                  setShowDate(false);
+                }}
+              />
+            )}
           </View>
         </View>
 
